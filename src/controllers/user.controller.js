e59bdc82cb1e4f095ba362756df79e323e706c3a
aliabbas827 <muhammadaliabbas827@gmail.com>
2024-03-23 12:16:01 +0500

    bugs fixed

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -17,7 +17,7 @@ const registerUser = asyncHandler(async (req, res) => {
 
     // check if user already exists
 
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or: [{ username }, { email }]
     })
 
@@ -28,13 +28,22 @@ const registerUser = asyncHandler(async (req, res) => {
 
     // check for images or avatar
     const avatarLocalPath = req.files?.avatar[0]?.path
-    const coverImageLocalPath = req.files?.coverImage[0]?.path
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path
+
+    let coverImageLocalPath;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0){
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
+
+
 
     if (!avatarLocalPath) {
         throw new ApiError(400, "avatar image is required")
     }
 
     // upload them to cloudinary
+
+    console.log(req.files);
 
     const avatar = await uploadOnCloudinary(avatarLocalPath)
     const coverImage = await uploadOnCloudinary(coverImageLocalPath)
